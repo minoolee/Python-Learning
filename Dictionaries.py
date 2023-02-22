d09b291ed0a45dfff1a2f0733a46abbf7cddb8b0
minoolee <yaqoubiamina@gmail.com>
2023-02-22 23:51:57 +0100

    Python is great.

--- Dictionaries.py
+++ Dictionaries.py
@@ -40,23 +40,79 @@ print(thisDict)
 print("************10***********")
 # Removing Items
 thisDict.pop("model")
-print("************27***********")
-print("************28***********")
+print(thisDict)
+print("************11***********")
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+thisDict.popitem()
+print(thisDict)
+print("************12***********")
+# The del keyword removes the item with the specified key name:
+del thisDict["year"]
+print(thisDict)
+print("************13***********")
+# The clear() method empties the dictionary:
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+print(car)
+print("************14***********")
+loopDic = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in loopDic:
+    print(x)
+print("************15***********")
+for x in loopDic:
+    print(loopDic[x])
 print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
+# ou can also use the values() method to return values of a dictionary:
+for x in loopDic.values():
+    print(x)
+print("************17***********")
+# You can use the keys() method to return the keys of a dictionary:
+for x in loopDic.keys():
+    print(x)
+print("************18***********")
+# Loop through both keys and values, by using the items() method:
+for x, y in loopDic.items():
+    print(x, y)
+print("************19***********")
+# Make a copy of a dictionary with the copy() method:
+copyDic = loopDic.copy()
+print(copyDic)
+print("************20***********")
+# Make a copy of a dictionary with the dict() function:
+dictDic = dict(loopDic)
+print(dictDic)
+print("************21***********")
+# Nested Dictionaries
+child1 = {
+    "name": "Emil",
+    "year": 2004
+}
+child2 = {
+    "name": "Tobias",
+    "year": 2007
+}
+child3 = {
+  "name": "Linus",
+  "year": 2011
+}
+myFamily = {
+    "child1": child1,
+    "child2": child2,
+    "child3": child3
+}
+print(myFamily)
+print("************22***********")
+print(myFamily["child2"]["name"])
+# Create a dictionary with 3 keys, all with the value 0:
+x = ('key1', 'key2', 'key3')
+y = 0
+newDic = dict.fromkeys(x, y)
+print(newDic)
---
+++ IfElse.py
@@ -0,0 +1,77 @@
+# Python If ... Else
+# Python Conditions and If statements
+# Python supports the usual logical conditions from mathematics:
+#
+#     Equals: a == b
+#     Not Equals: a != b
+#     Less than: a < b
+#     Less than or equal to: a <= b
+#     Greater than: a > b
+#     Greater than or equal to: a >= b
+print("************1***********")
+a = 7
+b = 14
+if b > a:
+    print("b is greater than a")
+print("************2***********")
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+c = 77
+d = 77
+if d > c:
+    print("d is greater than a")
+elif c == d:
+    print("c and d are equal")
+print("************3***********")
+# The else keyword catches anything which isn't caught by the preceding conditions.
+m = 77
+h = 55
+if h > m:
+    print("h is greater than m")
+elif m == h:
+    print("m and h are equal")
+else:
+    print("m is greater than h")
+print("************4***********")
+# Short Hand If
+if m > h: print("m is greater than h")
+print("************5***********")
+# Short Hand If ... Else
+print("M") if m > h else print("H")
+# This technique is known as Ternary Operators, or Conditional Expressions.
+print("************6***********")
+
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
