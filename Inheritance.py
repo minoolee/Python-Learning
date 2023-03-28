5f129c64c778d73c5191a7a177f1087875563aa8
minoolee <yaqoubiamina@gmail.com>
2023-03-28 12:09:31 +0200

    Python is great.

--- Inheritance.py
+++ Inheritance.py
@@ -14,35 +14,58 @@ class Person:
 
     # Use the Person class to create an object, and then execute the printname method:
 
+
 person1 = Person("Tom", "Krus")
 person1.printname()
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************27***********")
-print("************28***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
+print("************1***********")
+#  Create a Child Class
+
+
+class Student(Person):
+    pass
+
+
+student1 = Student("Mike", "Jordon")
+student1.printname()
+print("************2***********")
+# Add the __init__() Function
+class Teacher(Person):
+    def __init__(self, fname, lname):
+     Person.__init__(self, fname, lname)
+teacher =Teacher("Same", "Simpson")
+teacher.printname()
+print("************3***********")
+# Use the super() Function
+# Python also has a super() function that will make the child class inherit
+# all the methods and properties from its parent:
+class Baby(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+
+baby = Baby("Dimedime", "Paq")
+baby.printname()
+print("************4***********")
+
+
+# Add Properties
+class Charsi(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduationyear = 2022
+
+
+charsi = Charsi("Hamzah", "Chars")
+print(charsi.graduationyear)
+print("************5***********")
+# Add Methods
+
+class AddMethods(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+moreInfo = AddMethods("Milisa", "Olson", 2022)
+moreInfo.welcome()
---
+++ Iterators.py
@@ -0,0 +1,82 @@
+# Python Iterators
+# An iterator is an object that contains a countable number of values.
+# An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+# Technically, in Python, an iterator is an object which
+# implements the iterator protocol, which consist of the methods __iter__() and __next__().
+
+
+# Iterator vs Iterable
+
+# Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which
+# you can get an iterator from.
+# All these objects have a iter() method which is used to get an iterator:
+
+print("************1***********")
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+print(next(myit))
+print(next(myit))
+print(next(myit))
+# print(next(myit)) StopIteration
+print("************2***********")
+myStr = "banana"
+myIt = iter(myStr)
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print(next(myIt))
+print("************3***********")
+# Create an Iterator
+
+# To create an object/class as an iterator you have to implement the methods __iter__() and __next__() to your object.
+# As you have learned in the Python Classes/Objects chapter,
+# all classes have a function called __init__(),
+# which allows you to do some initializing when the object is being created.
+# The __iter__() method acts similar, you can do operations (initializing etc.),
+# but must always return the iterator object itself.
+# The __next__() method also allows you to do operations, and must return the next item in the sequence.
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+myClass = MyNumbers()
+myIter = iter(myClass)
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
+print("************27***********")
+print("************28***********")
+print("************16***********")
+print("************16***********")
+print("************16***********")
