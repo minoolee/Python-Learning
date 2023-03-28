e17ad7216b08812b5a053dfaa53864e983d28e26
minoolee <yaqoubiamina@gmail.com>
2023-03-28 23:54:29 +0200

    Python is great.

--- Iterators.py
+++ Iterators.py
@@ -58,10 +58,93 @@ print(next(myIter))
 print(next(myIter))
 print(next(myIter))
 print(next(myIter))
-print("************16***********")
-print("************16***********")
-print("************16***********")
-print("************16***********")
+print("************4***********")
+# for item in c
+# 1) Let c_item = c.__iter__(), which defines iteration over a Cafe object
+# 2) Call c_item.__next__() to get the next value in the iteration process
+# 3) Repeat Ste 2 until we get StopIteration
+
+
+class Cafe:
+    def __init__(self, menu):
+        self._menu = menu
+
+    def __iter__(self):
+        self.i = -1
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i < len(self._menu):
+            return self._menu[self.i]
+        raise StopIteration
+
+
+c = Cafe(['Mocha Latte', 'Iced Coffee', 'Tee'])
+for item in c:
+    print(item)
+print("************5***********")
+# So usually, we prefer generators instead
+
+
+class Restaurant:
+    def __init__(self, menu):
+        self._menu = menu
+
+    def __iter__(self):
+        for item in self._menu:
+            yield  item
+
+
+r = Restaurant(['Pizza', 'Burger', 'Ice'])
+for item in r:
+    print(item)
+print("************6***********")
+# Ranger
+class FRange:
+    def __init__(self, start=0.0, stop=0.0, step=1.0):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    # Start
+    def __iter__(self):
+        self.value = self.start - self.step
+        return self
+
+    def __next__(self):
+        if self.value + self.step < self.stop:
+            self.value += self.step
+            return self.value
+        else:
+            raise StopIteration
+
+fr = FRange(0, 2, 0.5)
+for x in fr:
+    print(x)
+print("************7***********")
+# loop in loop
+class FRange2D:
+    def __init__(self, start=0.0, stop=0.0, step=1.0, rows=5):
+        self.rows = rows
+        self.fr = FRange(start,stop,step)
+
+    def __iter__(self):
+        self.value = 0
+        return self
+
+    def __next__(self):
+        if self.value < self.rows:
+            self.value += 1
+            return iter(self.fr)
+        else:
+            raise StopIteration
+
+fr = FRange2D(0, 2, 0.5, 4)
+for row in fr:
+    for x in row:
+        print(x, end=" ")
+    print()
 print("************16***********")
 print("************16***********")
 print("************16***********")
